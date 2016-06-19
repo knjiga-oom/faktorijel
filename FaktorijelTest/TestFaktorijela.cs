82b1ebb2149bbbacb0808fb56ea1027d0e5bfebf
julijan <julijan.sribar@gmail.com>
2016-06-20 00:42:54 +0200

    Početna implementacija Faktorijel

---
+++ FaktorijelTest/TestFaktorijela.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Matematika;
+
+namespace TestFunkcija
+{
+    [TestClass]
+    public class TestFaktorijela
+    {
+        [TestMethod]
+        public void Faktorijel_VraćaIspravanRezultatZaPrirodniBroj()
+        {
+            Assert.AreEqual(1, Funkcije.Faktorijel(1));
+            Assert.AreEqual(2, Funkcije.Faktorijel(2));
+            Assert.AreEqual(6, Funkcije.Faktorijel(3));
+            Assert.AreEqual(24, Funkcije.Faktorijel(4));
+            Assert.AreEqual(5040, Funkcije.Faktorijel(7));
+        }
+
+        // ovdje ćemo dodati ostale testne metode... 
+        [TestMethod]
+        public void Faktorijel_Vraća1ZaArgument0()
+        {
+            Assert.AreEqual(1, Funkcije.Faktorijel(0));
+        }
+
+        [TestMethod]
+        public void Faktorijel_BacaIznimkuZaNegativniArgument()
+        {
+            try
+            {
+                Funkcije.Faktorijel(-1);
+                Assert.Fail("Funkcija je trebala baciti iznimku");
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+            }
+        }
+    }
+}
---
+++ Matematika/Funkcije.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matematika
+{
+    public static class Funkcije
+    {
+        public static long Faktorijel(int broj)
+        {
+            if (broj < 0)
+                throw new ArgumentOutOfRangeException(nameof(broj));
+            if (broj == 0)
+                return 1;
+            long rezultat = broj;
+            while (--broj > 1)
+                rezultat *= broj;
+            return rezultat;
+        }
+    }
+}
